c7422542fa13a09e393e181badc38cfa640d0875
totosuki <e231kari@gmail.com>
2024-07-12 08:22:38 +0900

    server.pyを関数化した

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
 import socket
 
-# AF_INET = IPv4
-# SOCK_STREAM = TCP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def launch_server() -> socket.socket:
+  # AF_INET = IPv4
+  # SOCK_STREAM = TCP
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.bind(("127.0.0.1", 12345))
+  sock.listen()
+  print("Server is listening...")
+  return sock
 
-sock.bind(("127.0.0.1", 12345))
+def listen_client(sock: socket.socket):
+  while True:
+    conn, addr = sock.accept()
+    print(f"Source IP Address: {addr}")
+    conn.send(b"Hello World!")
 
-sock.listen()
+def main():
+  sock = launch_server()
+  listen_client(sock)
 
-print("Server is listening...")
-
-while True:
-  conn, addr = sock.accept()
-  
-  print(f"Source IP Address: {addr}")
-  
-  # クライアントのソケットにデータを送信
-  conn.send(b"Hello World!")
+main()
