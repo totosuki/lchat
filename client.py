38118d500cf08454b890f7c2deea006ec3334c9b
totosuki <e231kari@gmail.com>
2024-07-12 11:44:39 +0900

    名前登録機能を追加した

--- client.py
+++ client.py
@@ -7,8 +7,19 @@ def connect_server() -> socket.socket:
     sock.connect(("127.0.0.1", 12346))
     return sock
 
+def check_username(sock: socket.socket):
+    # username状況を確認
+    data = sock.recv(256).decode("utf-8")
+    
+    if data == "None":
+        name = input("名前を入力してください: ")
+        sock.send(name.encode("utf-8"))
+    else:
+        print(f"{data}さん、こんにちは")
+
 def main():
     sock = connect_server()
+    check_username(sock)
     sock.close()
 
 main()
--- server.py
+++ server.py
@@ -1,22 +1,35 @@
 import socket
+from collections import defaultdict
 
 def launch_server() -> socket.socket:
-  # AF_INET = IPv4
-  # SOCK_STREAM = TCP
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.bind(("127.0.0.1", 12345))
-  sock.listen()
-  print("Server is listening...")
-  return sock
+    # AF_INET = IPv4
+    # SOCK_STREAM = TCP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("127.0.0.1", 12346))
+    sock.listen()
+    print("Server is listening...")
+    return sock
+
+def set_username(sock: socket.socket, conn: socket.socket) -> str:
+    conn.send("None".encode("utf-8")) # 0: 名前が決まっている, 1: 名前が決まってない
+    name = conn.recv(1024).decode("utf-8")
+    return name
 
 def listen_client(sock: socket.socket):
-  while True:
-    conn, addr = sock.accept()
-    print(f"Source IP Address: {addr}")
-    conn.send(b"Hello World!")
+    namedict = defaultdict(str)
+    while True:
+        conn, addr = sock.accept()
+        ip, _ = addr
+    
+        if not namedict[ip]:
+            namedict[ip] = set_username(sock, conn)
+            print(f"[Chat] {namedict[ip]}さんが初めて接続しました！")
+        else:
+            conn.send(namedict[ip].encode("utf-8"))
+            print(f"[Chat] {namedict[ip]}さんが接続しました！")
 
 def main():
-  sock = launch_server()
-  listen_client(sock)
+    sock = launch_server()
+    listen_client(sock)
 
 main()
