2d78190923ec22c199c07f5904aa55d4cdc303be
totosuki <e231kari@gmail.com>
2024-07-12 11:24:46 +0900

    client.pyを関数化した

--- client.py
+++ client.py
@@ -1,15 +1,14 @@
 import socket
 
-# AF_INET = IPv4
-# SOCK_STREAM = TCP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def connect_server() -> socket.socket:
+    # AF_INET = IPv4
+    # SOCK_STREAM = TCP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("127.0.0.1", 12346))
+    return sock
 
-sock.connect(("127.0.0.1", 12345))
+def main():
+    sock = connect_server()
+    sock.close()
 
-# サーバーからデータ受信
-data = sock.recv(4096)
-
-print(data)
-
-# クライアントのソケットを削除する
-sock.close()
+main()
