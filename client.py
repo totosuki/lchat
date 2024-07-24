2406f73e16d44d44bb4d416156bfe404983e75d2
totosuki <e231kari@gmail.com>
2024-07-24 23:31:20 +0900

    エラー処理やスレッド化、変数名の調整などした

--- client.py
+++ client.py
@@ -1,10 +1,15 @@
 import socket
+import threading
 
 def connect_server() -> socket.socket:
     # AF_INET = IPv4
     # SOCK_STREAM = TCP
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("127.0.0.1", 12346))
+    try:
+        sock.connect(("127.0.0.1", 12345))
+    except socket.error as err:
+        print(f"[Error] サーバーに接続できませんでした: {err}")
+        exit()
     return sock
 
 def check_username(sock: socket.socket):
@@ -17,9 +22,28 @@ def check_username(sock: socket.socket):
     else:
         print(f"{data}さん、こんにちは")
 
+def receive_messages(sock: socket.socket):
+    while True:
+        data = sock.recv(256).decode("utf-8")
+        print(data)
+
+def chat(client_socket: socket.socket):
+    while True:
+        try:
+            message = input()
+            client_socket.send(message.encode("utf-8"))
+        except socket.error as err:
+            print(f"[Error] 送信エラー: {err}")
+            client_socket.close()
+            break
+
 def main():
-    sock = connect_server()
-    check_username(sock)
-    sock.close()
+    client_socket = connect_server()
+    check_username(client_socket)
+    
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    receive_thread.start()
+    
+    chat(client_socket)
 
 main()
