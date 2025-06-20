529531e568ec2aaaee896cf6b0928184d25b677e
Totosuki <e231kari@gmail.com>
2025-06-20 16:14:31 +0900

    送信するデータを構造体にした

---
+++ lchat/src/lib.rs
@@ -0,0 +1,71 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum PacketType {
+    NicknameRequest,
+    NicknameResponse,
+    Message,
+    Join,
+    Leave,
+    Error,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Packet {
+    pub packet_type: PacketType,
+    pub content: String,
+    pub nickname: Option<String>,
+}
+
+impl Packet {
+    pub fn new(packet_type: PacketType, content: String, nickname: Option<String>) -> Self {
+        Self {
+            packet_type,
+            content,
+            nickname,
+        }
+    }
+
+    pub fn nickname_request() -> Self {
+        Self::new(PacketType::NicknameRequest, "Please enter your nickname: ".to_string(), None)
+    }
+
+    pub fn nickname_response(nickname: String) -> Self {
+        Self::new(PacketType::NicknameResponse, nickname, None)
+    }
+
+    pub fn message(content: String, nickname: String) -> Self {
+        Self::new(PacketType::Message, content, Some(nickname))
+    }
+
+    pub fn join(nickname: String) -> Self {
+        Self::new(PacketType::Join, format!("*** {} joined ***", nickname), Some(nickname))
+    }
+
+    pub fn leave(nickname: String) -> Self {
+        Self::new(PacketType::Leave, format!("*** {} left ***", nickname), Some(nickname))
+    }
+
+    pub fn error(content: String) -> Self {
+        Self::new(PacketType::Error, content, None)
+    }
+
+    pub fn to_json(&self) -> Result<String, serde_json::Error> {
+        serde_json::to_string(self)
+    }
+
+    pub fn from_json(json: &str) -> Result<Self, serde_json::Error> {
+        serde_json::from_str(json)
+    }
+
+    pub fn log(&self) {
+        match self.packet_type {
+            PacketType::NicknameRequest => println!("[NicknameRequest] : {}", self.content),
+            PacketType::NicknameResponse => println!("[NicknameResponse] : {}", self.content),
+            PacketType::Message => println!("[Message] {} : {}", self.nickname.as_deref().unwrap_or("Unknown"), self.content),
+            PacketType::Join => println!("[Join] {}", self.content),
+            PacketType::Leave => println!("[Leave] {}", self.content),
+            PacketType::Error => eprintln!("[Error] {}", self.content),
+        }
+    }
+}
